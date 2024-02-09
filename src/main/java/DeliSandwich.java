29eece8c929560953f2439bca7376ce10b0844b7
Dr. Drew Clinkenbeard <dclinkenbeard@csumb.edu>
2024-02-09 09:18:07 -0800

    added starter code for DeliSandWhich and fixed a typo

---
+++ src/main/java/DeliSandwich.java
@@ -0,0 +1,30 @@
+/**
+ * Used in a lesson on inheritance.
+ */
+public class DeliSandwich {
+
+  //Integer rather than int becuase of containers.
+  private Integer slicesOfBread;
+  private String filling;
+
+  public DeliSandwich(Integer slicesOfBread, String filling) {
+    this.slicesOfBread = slicesOfBread;
+    this.filling = filling;
+  }
+
+  /**
+   * Using a StringBuilder because we will use a loop later...
+   * @return a String about sandwiches
+   */
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("This sandwhich has ");
+    sb.append(filling);
+    sb.append(" between ");
+    sb.append(slicesOfBread);
+    sb.append(" slices of bread.");
+    return sb.toString();
+  }
+}
+
